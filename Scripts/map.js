4a3b59d9d4dfde8996535b8de0298c049dabb0ce
claudiu725 <claudiu725@yahoo.com>
2017-03-12 03:24:18 +0200

    added percentage code

--- Scripts/map.js
+++ Scripts/map.js
@@ -8,7 +8,29 @@ document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function () {
     $_GET[decode(arguments[1])] = decode(arguments[2]);
 });
 
+//http://stackoverflow.com/a/3642265/1869660
+function makeSVGElement(tag, attrs) {
+    var el= document.createElementNS('http://www.w3.org/2000/svg', tag);
+    for (var k in attrs) {
+        el.setAttribute(k, attrs[k]);
+    }
+    return el;
+}
+
+var globalPath = [];
+var t = 0;
+var listofpnts2 = [];
+
+var distanceBetweenCircles = 1;
+var timerPeriodMs = 50;
+var timerIncrement = 5;
+var perStep = 0.02; // 0 <= per <= 1
+
 $( document ).ready(function() {
+	$("#mapContainer").click(function(){
+		per += perStep;
+	});
+	
 	$( "#mapContainer" ).load($_GET["map"] + '.svg', function(){
 		console.log('debug: ' +
 				'<br>wp ' + JSON.stringify(wp));
@@ -22,23 +44,79 @@ $( document ).ready(function() {
 		'<br>dest ' +  $_GET["dest"] + 
 		'<br>shortestpath: ' + shortest);
 		
-		var last = null;
+		var pnts = "";
+		var listofpnts = [];
 		jQuery.each(shortest, function(idx, val){
-			if (last != null) {
-				var firstWay = $('#' + last);
-				var secondWay = $('#' + val);
-			$(document.createElementNS('http://www.w3.org/2000/svg','line')).attr({
-			  x1:firstWay.attr("cx"),
-			  y1:firstWay.attr("cy"),
-			  x2:secondWay.attr("cx"),
-			  y2:secondWay.attr("cy"),
-			  'stroke-width': 2,
-			  'stroke-dasharray': 2.5,
-			  'stroke-linecap': 'round',
-			  stroke: 'red'
-			  }).appendTo($("svg"));
+			var ch = 'L';
+			if (idx == 0)
+			{
+				ch = 'M';
 			}
-			last = val;
+			if (idx == shortest.length)
+			{
+				ch = 'Z';
+			}
+			var wayp = $('#' + val);
+			pnts += ch + ' ' + wayp.attr("cx") + ' ' + wayp.attr("cy");
+			var x = wayp.attr("cx");
+			var y = wayp.attr("cy");
+			listofpnts.push({ 'x' : x , 'y' : y});
 		});
+		var globalPath2 = $(document.createElementNS('http://www.w3.org/2000/svg','path')).attr({
+		  d : pnts,
+		  fill : 'none',
+          'stroke-width': 2,
+		  'stroke-dasharray': 2.5,
+          //'stroke-dasharray': 50,
+		  'stroke-linecap': 'round',
+		  'stroke-opacity': 1,
+		  stroke: 'red'
+		 }).appendTo($("svg"));
+		
+		var lastp;
+		jQuery.each(listofpnts, function(idx, val){
+			if (idx != 0)
+			{
+				var mod = Math.sqrt(Math.pow( lastp.x - val.x, 2) + Math.pow( lastp.y - val.y, 2));
+				var i;
+				for (i = 0.0; i <= mod; i += distanceBetweenCircles)
+				{
+					var proc = i / mod;
+					listofpnts2.push({ 'x' : lastp.x * (1 - proc) + val.x * proc, 'y' : lastp.y * (1 - proc) + val.y * proc });
+				}
+				i -= mod;
+			}
+			lastp = val;
+		});
+		
+		var intervalID = setInterval(function(){
+			//per = t / 100.0;
+		    t += timerIncrement;
+		    if (t > 100)
+		    	t = 0;
+		    var sliced = globalPath.slice(0, -10);
+		    var i;
+		    $(".removable").remove();
+		    jQuery.each(listofpnts2, function(idx, val){
+			var fillit = Math.abs(idx - (listofpnts2.length * per)) < 2;
+			var filltype = 'none';
+			if (fillit)
+			{
+				filltype = '#0f0';
+				var el = $(document.createElementNS('http://www.w3.org/2000/svg','circle')).attr({
+				  class : 'removable',
+				  cx : val.x,
+				  cy : val.y,
+				  r : 5,
+				  fill : filltype,
+		          //'stroke-dasharray': 50,
+				  'stroke-linecap': 'round',
+				  'stroke-opacity': 0,
+				  stroke: 'red'
+				 });
+				$("svg").append(el);
+			}
+		});
+		}, timerPeriodMs);
 	});
 });

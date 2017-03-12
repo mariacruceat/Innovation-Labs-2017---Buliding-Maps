0622b5506e7ef14845cc6b761050f89ebb65d8c7
mariacruceat <cruceat.maria@gmail.com>
2017-03-12 06:57:11 +0200

    cc

--- Scripts/map.js
+++ Scripts/map.js
@@ -125,7 +125,7 @@ $( document ).ready(function() {
 	});
 
 	var myShakeEvent = new Shake({
-		//threshold: 15, // optional shake strength threshold
+		threshold: 10, // optional shake strength threshold
 		//timeout: 1000 // optional, determines the frequency of event generation
 	});
 	myShakeEvent.start();
